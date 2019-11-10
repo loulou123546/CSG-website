b237e48eab3c22178b221269032be0186544eb44
rubenio1 <ruben.barkman@hotmail.com>
2019-11-10 23:42:38 +0100

    Weebly Update

--- html/consecgroup.weebly.com/OldRegister/db.php
+++
@@ -1,7 +0,0 @@
-<?php
-$con = mysqli_connect("localhost","login","loginCSGCoder2015","register");
-if (mysqli_connect_errno())
-  {
-  echo "Failed to connect to MySQL: " . mysqli_connect_error();
-  }
-?>
--- html/consecgroup.weebly.com/OldRegister2/README.md
+++
@@ -1,2 +0,0 @@
-# useraccounts
-User Registration Form with PHP and MySQL Tutorial code
